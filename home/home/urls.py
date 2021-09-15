68029b432e8cafd01b51bd247376b66f30458b11
Kolya <amigo.bro.657@gmail.com>
2021-09-15 11:12:06 +0300

    first page

--- home/home/urls.py
+++ home/home/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('page.urls')),
+    path('home/', include('page.urls')),
+    # path('price/', include()),
+    path("", lambda request: redirect("/home/")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/page/urls.py
+++ home/page/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from page.views import home_page
 
-
-
 urlpatterns = [
     path("", home_page, name="home_page"),
 ]
